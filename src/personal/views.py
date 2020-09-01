538875af52d9314746446d9409c15eaf8b8a35a1
vinosamari <iamvinomusic@gmail.com>
2020-09-01 03:17:32 +0100

    Created Blog Model
    Blog page not created yet

--- src/personal/views.py
+++ src/personal/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from account.models import Account
+from blog.models import BlogPost
 
 
 # Create your views here.
@@ -7,6 +8,9 @@ def home_screen_view(request):
     context = {}
     
     accounts = Account.objects.all()
+    blogs = BlogPost.objects.all()
+     
     context['accounts'] = accounts
+    context['blogs'] = blogs
     
     return render(request, 'personal/home.html', context)
